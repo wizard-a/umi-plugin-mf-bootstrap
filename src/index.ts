c1ad07c5a9f92d1fe6e183ef661632d79c8ae077
sunfangyuan <sunfangyuan@baijia.com>
2021-07-19 15:33:49 +0800

    fix: Fix the path problem of the plugin during build

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import { resolve } from 'path';
 import { readFileSync } from 'fs';
 
 export default (api: IApi) => {
-
   api.onGenerateFiles(() => {
-    const buffer= readFileSync(resolve('./src/.umi/umi.ts'))
-    const c = String(buffer)
+    const path =
+      api.env === 'production' ? './src/.umi/index.ts' : './src/.umi/umi.ts';
+    const buffer = readFileSync(resolve(path));
+    const c = String(buffer);
     // console.log()
     api.writeTmpFile({
       path: 'index.ts',
