5c6c33db3bdab50ee25c08d49c85dc8223d89dd4
happy-func <stevenhanth@gmail.com>
2022-03-10 23:39:50 +0800

    Fix ./src/.umi/index.ts or ./src/.umi/index.ts file does not exist causing error via docker build

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 module-federation 异步启动插件
 
-
 ### change log
 
-* 0.0.2
-1. Fix the path problem of the plugin during build
+- 0.0.3
+
+1.  Fix `./src/.umi/index.ts` or `./src/.umi/index.ts` file does not exist causing error via docker build
--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,17 @@ import { readFileSync } from 'fs';
 
 export default (api: IApi) => {
   api.onGenerateFiles(() => {
-    const path =
-      api.env === 'production' ? './src/.umi/index.ts' : './src/.umi/umi.ts';
-    const buffer = readFileSync(resolve(path));
-    const c = String(buffer);
-    // console.log()
+    let c = '';
+    try {
+      const path =
+        api.env === 'production' ? './src/.umi/index.ts' : './src/.umi/umi.ts';
+      const buffer = readFileSync(resolve(path));
+      c = String(buffer);
+    } catch (e) {
+      console.info(
+        `src/.umi/index.ts or src/.umi/umi.ts it's not defined, set empty content to tmp file`,
+      );
+    }
     api.writeTmpFile({
       path: 'index.ts',
       content: c,
